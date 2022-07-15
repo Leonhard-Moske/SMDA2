705e86b819e0310c879d37d3b8de346e33765d1b
Leonhard-Moske <leonhardmoske@gmail.com>
2022-07-15 10:46:02 +0200

    kolmogorov-Smirnov test

--- termPaper/projectCode/dataExploration.py
+++ termPaper/projectCode/dataExploration.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 x_data, y_data = astroML.datasets.fetch_rrlyrae_combined(data_home="/tmp/astroML_data", download_if_missing=True)
 
 #histogramms split by label
+#------------------------------------------------------------------------------
 for i in range(len(x_data[0])):
     plt.hist(x_data[y_data==0,i], bins= 100, label="background star", alpha=0.7, color = "tab:orange")
     plt.hist(x_data[y_data==1,i], bins= 100, label="RR Lyrae", alpha=1, color = "tab:blue")
@@ -15,6 +16,7 @@ for i in range(len(x_data[0])):
     plt.clf()
 
 #feature histogramm
+#------------------------------------------------------------------------------
 for i in range(len(x_data[0])):
     plt.hist(x_data[:,i], bins= 100)
     plt.xlabel(f"feature {i}") #todo featureliste
@@ -22,7 +24,15 @@ for i in range(len(x_data[0])):
     plt.savefig(f"figs/hist_xData_feature{i}.png",format="png")
     plt.clf()
 
+#Kolmogorov-Smirnov test
+#------------------------------------------------------------------------------
+#high value is indicator for good feature
+from scipy.stats import ks_2samp
 
-#todo normale distribution
+for i in range(len(x_data[0])):
+    print(f"feature {i}\t",ks_2samp(x_data[y_data==0,i], x_data[y_data==1,i]))
+
+
+
+
 #todo correlation plots in 4 x 4 grid
-#todo kolmogorov metric
