1309854a83f783be292e9f4c8bb1a646ea2e2fd4
Leonhard-Moske <leonhardmoske@gmail.com>
2022-07-15 10:33:19 +0200

    feature histogramms

--- termPaper/projectCode/dataExploration.py
+++ termPaper/projectCode/dataExploration.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 x_data, y_data = astroML.datasets.fetch_rrlyrae_combined(data_home="/tmp/astroML_data", download_if_missing=True)
 
-
+#histogramms split by label
 for i in range(len(x_data[0])):
     plt.hist(x_data[y_data==0,i], bins= 100, label="background star", alpha=0.7, color = "tab:orange")
     plt.hist(x_data[y_data==1,i], bins= 100, label="RR Lyrae", alpha=1, color = "tab:blue")
@@ -11,7 +11,15 @@ for i in range(len(x_data[0])):
     plt.xlabel(f"feature {i}") #todo featureliste
     plt.ylabel("Number of stars")
     plt.legend()
-    plt.savefig(f"figs/hist_xData_feature_labelSplit{i}.png",format="png")
+    plt.savefig(f"figs/hist_xData_feature{i}_labelSplit.png",format="png")
+    plt.clf()
+
+#feature histogramm
+for i in range(len(x_data[0])):
+    plt.hist(x_data[:,i], bins= 100)
+    plt.xlabel(f"feature {i}") #todo featureliste
+    plt.ylabel("Number of stars")
+    plt.savefig(f"figs/hist_xData_feature{i}.png",format="png")
     plt.clf()
 
 
